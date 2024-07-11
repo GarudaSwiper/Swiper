a709dd4862f44a459c259a43e7996549fd20a866
BebekJK <kevinjk2802@gmail.com>
2024-07-12 03:04:00 +0800

    feat: clean up code

---
+++ ai/main.py
@@ -0,0 +1,15 @@
+from video_splitter import extract_all
+
+if __name__ == "__main__":
+    # Extract the video's frames, audio, and transcript
+    # @ Frame extraction for emotion detection
+    # @ Audio extraction for transcription
+    # @ Transcript extraction for sentiment analysis
+    
+    video_path = 'ai/videos/_Kevin Surace- 1 Minute Ted Talk.mp4'
+    video_name = 'ai/results/frame.mp4'
+    audio_name = 'ai/results/audio.wav'
+    transcript_name = 'ai/results/transcript.txt'
+    
+    # extract_all(video_path, video_name, audio_name, transcript_name)
+    
